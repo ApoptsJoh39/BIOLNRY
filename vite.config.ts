fb44f4bf42a90bb57c10e607301622d5e32b7dba
ApoptsJoh39 <159179218+ApoptsJoh39@users.noreply.github.com>
2025-11-10 02:50:18 -0600

    Fix: Corregir la configuración de Vite y Django

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
+    // Relative to the project root
     outDir: './static/dist',
     rollupOptions: {
       input: {
-        main: './static_src/src/main.tsx',
+        // This is your CSS entry point
+        styles: './static_src/src/input.css',
       },
+      output: {
+        // This will output a styles.css file
+        assetFileNames: '[name].css'
+      }
     },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
   },
 });
