561e77429f231373088a2f69990e3dd383c0e1ba
ApoptsJoh39 <159179218+ApoptsJoh39@users.noreply.github.com>
2025-11-09 22:29:29 -0600

    Fix(tailwind): Correct content path to scan all app templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/**/*.html',
-    './static_src/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}', // Para archivos React/Vite
-    './frontend/**/*.{js,ts,jsx,tsx}', // Si usas otra estructura
-    './**/templates/**/*.html' // Rutas adicionales
+    './**/*.html'
   ],
   theme: {
     extend: {},
@@ -15,6 +11,7 @@ module.exports = {
           'light': '#A8324A',     // Un tono más claro, como afrutado
           'DEFAULT': '#800020',   // El clásico color Borgoña/vino
           'dark': '#5B0016',      // Un tono de vino profundo y oscuro
+
         },
       }
     },
